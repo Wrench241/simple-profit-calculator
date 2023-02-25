8f08f2965f262716cfafb3c08ce3eaabf668dfcc
Octavius <121876628+Wrench241@users.noreply.github.com>
2023-02-25 17:24:11 -0300

    Add files via upload

---
+++ calculadora lucro.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+public class pratica {
+    public static void main(String[] args) {
+
+       float bar = Float.parseFloat(JOptionPane.showInputDialog(null,"digite valor gasto com material:",
+        "calculadora de lucros",
+        JOptionPane.INFORMATION_MESSAGE ));
+
+      float bar4 = Float.parseFloat(JOptionPane.showInputDialog(null,"Digite quanto voce recuperou de dinheiro:",
+                                                            "calculadora de lucros",
+                                                        JOptionPane.INFORMATION_MESSAGE));
+     float bar2 = bar4 - bar;
+
+     if (bar4 <= bar) {
+
+        JOptionPane.showMessageDialog(null,"VOCÊ NÃO OBTEVE LUCRO AINDA",
+        "calculadora de lucros",
+        JOptionPane.INFORMATION_MESSAGE);
+
+     } else if (bar4 > bar) {
+
+        JOptionPane.showMessageDialog(null,String.format("SEU LUCRO TOTAL FOI DE %.2f$",bar2),
+        "calculadora de lucros",
+        JOptionPane.INFORMATION_MESSAGE);
+     }
+
+    }
+}
